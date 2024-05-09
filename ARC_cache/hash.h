cac2be31f8deadd0705ba0662981ba704e6cc4b5
Ve-Po <popova.eiu@phystech.edu>
2024-05-09 17:17:49 +0300

    header for hash

--- ARC_cache/hash.h
+++ ARC_cache/hash.h
@@ -2,38 +2,41 @@
 #define HASH_TABLE
 
 
-// Определение enum для списков
-typedef enum {
-    T1,
-    T2,
-    B1,
-    B2
-} ListType;
-
-// Определение структуры элемента хэш-таблицы
-typedef struct HashItem {
-    int key;
-    ListType value;
-} HashItem;
-
-// Определение структуры хэш-таблицы
-typedef struct HashTable {
-    HashItem** items;
-    int capacity;
-} HashTable;
-
-// Функция для создания нового элемента хэш-таблицы
-HashItem* newHashItem(int key, ListType value);
-// Функция для создания новой хэш-таблицы
-HashTable* newHashTable(int capacity);
-// Функция для добавления элемента в хэш-таблицу
-void hashTableAdd(HashTable* table, int key, ListType value);
-// Функция для вывода содержимого хэш-таблицы
-void printHashTable(HashTable* table);
-// Функция для обновления значения элемента в хэш-таблице
-void hashTableUpdate(HashTable* table, int key, ListType newValue);
-////////
-ListType getHashTableValue(HashTable* table, int key);
+typedef struct hashnode hashnode;
+typedef struct Hash Hash;
 
 
+
+struct hashnode
+{
+	int value;
+	node * point;
+	hashnode * next;
+	hashnode * prev;
+};
+
+struct Hash
+{
+	int capacity;
+	hashnode **array;
+};
+
+//create hashnode
+hashnode* create_hashnode(int value, node *point, hashnode *prev);
+//create array
+hashnode **init_array(int capacity);
+//create hashtable
+Hash* create_HASH_TABLE(int capacity);
+//add node to hash
+Hash* hashTableAdd(Hash *table, node *point, int value);
+//search for element in hashtable
+hashnode* find_element_in_hash(Hash *hash, int page);
+//hash function
+int hash_function(int value, Hash *table);
+//free the hash branch
+Hash* free_the_branch(hashnode *tmp);
+//free hash table
+void free_HASH(Hash *hash);
+//delete element from hash table
+void hash_delete_elem(int page, Hash *s);
 #endif
