d95c2feca3bee8466077ed14231e4c9d9ac63b9d
Ve-Po <popova.eiu@phystech.edu>
2024-05-09 17:08:50 +0300

    create hashtable

--- ARC_cache/hash.cpp
+++ ARC_cache/hash.cpp
@@ -4,62 +4,166 @@
 #include "hash.h"
 
 
-// Функция для создания нового элемента хэш-таблицы
-HashItem* newHashItem(int key, ListType value) {
-    HashItem* item = (HashItem*)malloc(sizeof(HashItem));
-    item->key = key;
-    item->value = value;
-    return item;
+//узел в хэш-таблице
+hashnode* create_hashnode(int value, node *point, hashnode *prev)
+{
+	hashnode *tmp = NULL;
+	tmp = (hashnode*) malloc(sizeof(hashnode));
+	tmp->value = value;
+	tmp->point = point;
+	tmp->prev = prev;
+	tmp->next = NULL;
+
+	return tmp;
+}
+
+
+//функция для инициализации массива в хэш таблице
+hashnode **init_array(int capacity)
+{
+	hashnode **array = NULL;
+	array = (hashnode **) malloc(capacity* sizeof(hashnode*));
+	for (int i = 0; i < capacity; i++)
+	{
+		(array)[i] = NULL;
+	}
+
+	return array;
+
 }
 
 // Функция для создания новой хэш-таблицы
-HashTable* newHashTable(int capacity) {
-    HashTable* table = (HashTable*)malloc(sizeof(HashTable));
-    table->items = (HashItem**)malloc(capacity * sizeof(HashItem*));
-    for (int i = 0; i < capacity; i++) {
-        table->items[i] = NULL;
-    }
-    table->capacity = capacity;
-    return table;
-}
+Hash* create_HASH_TABLE(int capacity)
+{
+	hashnode **array = NULL;
+	Hash *our_hash = NULL;
+	our_hash = (Hash*) malloc(sizeof(Hash));
+	our_hash->capacity = capacity;
+	array = init_array(capacity);
+	our_hash->array = array;
 
+	return our_hash;
+}
 
 // Функция для добавления элемента в хэш-таблицу
-void hashTableAdd(HashTable* table, int key, ListType value) {
-    int index = key % table->capacity;
-    HashItem* item = newHashItem(key, value);
-    table->items[index] = item;
-}
-
-// Функция для вывода содержимого хэш-таблицы
-void printHashTable(HashTable* table) {
-    printf("Содержимое хэш-таблицы: ");
-    for (int i = 0; i < table->capacity; i++) {
-        if (table->items[i] != NULL) {
-            printf("%d (%d) ", table->items[i]->key, table->items[i]->value);
-        }
-    }
-    printf("\n");
-}
-
-// Функция для обновления значения элемента в хэш-таблице
-void hashTableUpdate(HashTable* table, int key, ListType newValue) {
-    int index = key % table->capacity;
-    HashItem* item = table->items[index];
-    if (item != NULL && item->key == key) {
-        item->value = newValue;
-    } else {
-        printf("Ключ %d не найден в хэш-таблице\n", key);
-    }
-}
-
-ListType getHashTableValue(HashTable* table, int key)
+Hash* hashTableAdd(Hash *table, node *point, int value)
 {
-    int index = key % table->capacity;
-    if(table->items[index] != NULL && table->items[index]->key == key)
-    {
-        return table->items[index]->value;
-    }
-    return ListType();
+	int index = hash_function(value, table);
+	hashnode *tmp = (table->array)[index];
+	if (tmp == NULL)
+	{
+		tmp = create_hashnode(value, point, NULL);
+	}
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+
+		tmp->next = create_hashnode(value, point, tmp);
+	}
+
+	(table->array)[index] = tmp;
+	return table;
 }
 
+//ищем элемент в хэш таблице 
+hashnode* find_element_in_hash(Hash *hash, int page)
+{
+	int pl = hash_function(page, hash);
+	hashnode *tmp = (hash->array)[pl];
+	while (tmp != NULL)
+	{
+		if ((tmp->value) == page)
+		{
+			return tmp;
+		}
+
+		tmp = tmp->next;
+	}
+
+	return NULL;
+}
+
+//hash-function
+int hash_function(int value, Hash *table)
+{
+	int index = value % (table->capacity);
+	return index;
+}
+
+Hash* free_the_branch(hashnode *tmp)
+{
+	if (tmp == NULL)
+	{
+		return;
+	}
+
+	free_the_branch(tmp->next);
+	free_item(tmp);
+}
+//free hash item
+void free_item(hashnode *node)
+{
+	node->point = NULL;
+	node->value = 0;
+	free(node);
+}
+
+//print hash
+void print_hash(Hash *hash)
+{
+	if (hash)
+	{
+		for (int i = 0; i < hash->capacity; i++)
+		{
+			hashnode *ptr = hash->array[i];
+			if (ptr)
+			{
+				while (ptr != NULL)
+				{
+					printf("%d", ptr->value);
+					ptr = ptr->next;
+				}
+			}
+		}
+	}
+}
+void hash_delete_elem(int page, Hash *s)
+{
+	hashnode * h_node;
+	hashnode * h_node_next;
+
+	h_node = find_element_in_hash(s, page);
+	if (h_node == NULL)
+		return;
+
+	if (h_node->prev == NULL)
+	{
+		if (h_node->next == NULL)
+		{
+			h_node->value = 0;
+			h_node->point = NULL;
+			return;
+		}
+
+		h_node_next = h_node->next;
+		*h_node = *(h_node->next);
+		h_node->prev = NULL;
+
+		free(h_node_next);
+
+		if (h_node->next != NULL)
+			h_node->next->prev = h_node;
+
+		return;
+	}
+
+	h_node->prev->next = h_node->next;
+
+	if (h_node->next != NULL)
+		h_node->next->prev = h_node->prev;
+
+	free(h_node);
+}
