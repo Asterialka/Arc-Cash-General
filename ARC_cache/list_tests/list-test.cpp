fc96fed5185ef0ec78a0bca3d6c4d513cf18a63a
Artem Shilov <142316977+Asterialka@users.noreply.github.com>
2024-05-18 03:20:47 +0300

    update tests for list

--- ARC_cache/list_tests/list-test.cpp
+++ ARC_cache/list_tests/list-test.cpp
@@ -3,17 +3,7 @@
 #include <assert.h>
 
 #include "list.cpp"
-
-#define TEST_LIST_SIZE 100
-
-#define TEST_NUMBER_1 1
-#define TEST_NUMBER_2 2
-#define TEST_NUMBER_3 3
-#define TEST_NUMBER_4 4
-#define TEST_NUMBER_5 5
-#define TEST_NUMBER_6 6
-
-#define SIZE_NUMBER 3
+#include "list-test.h"
 
 
 int main()
@@ -21,6 +11,7 @@ int main()
     LinkedList* test_list = List(TEST_LIST_SIZE);
 
     assert(test_list->head == NULL);
+    assert(test_list->tail == NULL);
     assert(test_list->size == TEST_LIST_SIZE);
 
     push_front(test_list, TEST_NUMBER_1);
@@ -33,14 +24,16 @@ int main()
     delete_element(test_list, TEST_NUMBER_3);
     
     assert(test_list->head->next->next == NULL);
-   
     assert(test_list->head->next->val == TEST_NUMBER_1 && test_list->head->val == TEST_NUMBER_2);
+    assert(test_list->tail->next == NULL);
+    assert(test_list->tail->prev->val == TEST_NUMBER_2 && test_list->tail->val == TEST_NUMBER_1);
 
     delete_element(test_list, TEST_NUMBER_1);
     delete_element(test_list, TEST_NUMBER_2);
     
+    print_list(test_list);
     assert(test_list->head == NULL);
-
+   
     push_front(test_list, TEST_NUMBER_4);
     push_front(test_list, TEST_NUMBER_5);
     push_front(test_list, TEST_NUMBER_6);
@@ -50,8 +43,9 @@ int main()
     delete_back_element(test_list);
 
     assert(test_list->now_size == SIZE_NUMBER - 1);
-    assert(test_list->head->next->next = NULL);
-    print_list(test_list);
+    assert(test_list->head->next->next == NULL);
+    assert(test_list->head->next->val == TEST_NUMBER_5 && test_list->head->val == TEST_NUMBER_6);
+    assert(test_list->tail->val == TEST_NUMBER_5 && test_list->tail->prev->val == TEST_NUMBER_6);
 
-    printf("All tests passed successfully");
+    printf("All tests passed successfully \n");
 }
---
+++ ARC_cache/list_tests/list-test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_LIST
+#define TEST_LIST
+
+#define TEST_LIST_SIZE 100
+
+#define TEST_NUMBER_1 1
+#define TEST_NUMBER_2 2
+#define TEST_NUMBER_3 3
+#define TEST_NUMBER_4 4
+#define TEST_NUMBER_5 5
+#define TEST_NUMBER_6 6
+
+#define SIZE_NUMBER 3
+
+#endif
