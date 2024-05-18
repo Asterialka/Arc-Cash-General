6f86f9f4a407a6b7169ca6ccc62312a768b0cdf1
qwerfiko <142624907+qwerfiko@users.noreply.github.com>
2024-05-18 23:50:13 +0300

    added test_performance, null and same_key

--- ARC_cache/hash_tests/hash_tests.cpp
+++ ARC_cache/hash_tests/hash_tests.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <ctime>
 #include "hash.h"
 
 // Функция для загрузки чисел из файла
@@ -227,6 +228,73 @@ void test_remove_middle_of_chain() {
     printf("test_remove_middle_of_chain passed.\n");
 }
 
+// Тест для проверки работы с нулевыми значениями
+void test_null_values()
+{
+    printf("Running test_null_values...\n");
+    Hash* hash_table = create_HASH_TABLE(10);
+    hashTableAdd(hash_table, NULL, 0);
+    assert(find_element_in_hash(hash_table, 0) != NULL);
+    hash_delete_elem(0, hash_table);
+    assert(find_element_in_hash(hash_table, 0) == NULL);
+    free_HASH(hash_table);
+    printf("test_null_values passed.\n");
+}
+
+// Тест для проверки добавления элементов с одинаковыми ключами, но разными значениями
+void test_same_key_different_values()
+{
+    printf("Running test_same_key_different_values...\n");
+    Hash* hash_table = create_HASH_TABLE(10);
+    hashTableAdd(hash_table, (node*)1, 5);
+    assert(find_element_in_hash(hash_table, 5)->point == (node*)1);
+    hashTableAdd(hash_table, (node*)2, 5);
+    assert(find_element_in_hash(hash_table, 5)->point == (node*)2);
+    hash_delete_elem(5, hash_table);
+    assert(find_element_in_hash(hash_table, 5) == NULL);
+    free_HASH(hash_table);
+    printf("test_same_key_different_values passed.\n");
+}
+
+// Тест для проверки производительности
+void test_performance()
+{
+    printf("Running test_performance...\n");
+    const int num_elements = 1000000;
+    Hash* hash_table = create_HASH_TABLE(num_elements * 2);
+
+    // Замеряем время добавления элементов
+    clock_t start = clock();
+    for (int i = 0; i < num_elements; ++i)
+    {
+        hashTableAdd(hash_table, NULL, i);
+    }
+    clock_t end = clock();
+    printf("Time taken to add %d elements: %f seconds\n", num_elements, (double)(end - start) / CLOCKS_PER_SEC);
+
+    // Замеряем время поиска элементов
+    start = clock();
+    for (int i = 0; i < num_elements; ++i)
+    {
+        assert(find_element_in_hash(hash_table, i) != NULL);
+    }
+    end = clock();
+    printf("Time taken to find %d elements: %f seconds\n", num_elements, (double)(end - start) / CLOCKS_PER_SEC);
+
+    // Замеряем время удаления элементов
+    start = clock();
+    for (int i = 0; i < num_elements; ++i)
+    {
+        hash_delete_elem(i, hash_table);
+    }
+    end = clock();
+    printf("Time taken to delete %d elements: %f seconds\n", num_elements, (double)(end - start) / CLOCKS_PER_SEC);
+
+    free_HASH(hash_table);
+    printf("test_performance passed.\n");
+}
+
+
 
 
 int main()
@@ -250,6 +318,10 @@ int main()
     test_repeated_elements();
     test_large_values();
     test_remove_middle_of_chain();
+    test_null_values();
+    test_same_key_different_values();
+    test_performance();
+
     free_HASH(hash_table);
 
     printf("All tests passed successfully.\n");
