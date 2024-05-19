f2d0080816853387ebc5374ee09ab700d6f2ecbd
Ve-Po <142323606+Ve-Po@users.noreply.github.com>
2024-05-19 19:23:16 +0300

    were changed names of functions

--- ARC_cache/list_tests/list-test.c
+++ ARC_cache/list_tests/list-test.c
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#include "../list.cpp"
+#include "../list.c"
 #include "list-test.h"
 
 void test1()
 {
-    LinkedList* test_list = List(TEST_LIST_SIZE);
+    LinkedList* test_list = init_List(TEST_LIST_SIZE);
 
     assert(test_list->head == NULL);
     assert(test_list->tail == NULL);
     assert(test_list->size == TEST_LIST_SIZE);
 
-    list_free(&test_list);
-    assert(test_list == NULL);
+    list_free(test_list);
 }
 
 void test2()
 {
-    LinkedList* test_list = List(TEST_LIST_SIZE);
+    LinkedList* test_list = init_List(TEST_LIST_SIZE);
 
-    push_front(test_list, TEST_NUMBER_1);
-    push_front(test_list, TEST_NUMBER_2);
-    push_front(test_list, TEST_NUMBER_3);
+    pushFront(test_list, TEST_NUMBER_1);
+    pushFront(test_list, TEST_NUMBER_2);
+    pushFront(test_list, TEST_NUMBER_3);
     assert(test_list->head != NULL);
     assert(test_list->head->next != NULL);
     assert(test_list->head->next->next != NULL);
@@ -35,33 +34,33 @@ void test2()
     assert(test_list->tail->next == NULL);
     assert(test_list->tail->prev->val == TEST_NUMBER_2 && test_list->tail->val == TEST_NUMBER_1);
 
-    list_free(&test_list);
+    list_free(test_list);
 }
 
 void test3()
 {
-    LinkedList* test_list = List(TEST_LIST_SIZE);
+    LinkedList* test_list = init_List(TEST_LIST_SIZE);
 
-    push_front(test_list, TEST_NUMBER_1);
-    push_front(test_list, TEST_NUMBER_2);
-    push_front(test_list, TEST_NUMBER_3);
+    pushFront(test_list, TEST_NUMBER_1);
+    pushFront(test_list, TEST_NUMBER_2);
+    pushFront(test_list, TEST_NUMBER_3);
    
     print_list(test_list);
 
-    list_free(&test_list);
+    list_free(test_list);
 } 
 
 void test4()
 {
-    LinkedList* test_list = List(TEST_LIST_SIZE);
+    LinkedList* test_list = init_List(TEST_LIST_SIZE);
 
-    push_front(test_list, TEST_NUMBER_4);
-    push_front(test_list, TEST_NUMBER_5);
-    push_front(test_list, TEST_NUMBER_6);
+    pushFront(test_list, TEST_NUMBER_4);
+    pushFront(test_list, TEST_NUMBER_5);
+    pushFront(test_list, TEST_NUMBER_6);
 
     assert(test_list->now_size == SIZE_NUMBER);
     
-    delete_back_element(test_list);
+    popBack(test_list);
 
     assert(test_list->now_size == SIZE_NUMBER - 1);
     assert(test_list->head->next->next == NULL);
@@ -70,24 +69,24 @@ void test4()
 
     print_list(test_list);
 
-    list_free(&test_list);
+    list_free(test_list);
 }
 
 void test5()
 {
-    LinkedList* test_list = List(TEST_LIST_SIZE);
+    LinkedList* test_list = init_List(TEST_LIST_SIZE);
 
-    push_front(test_list, TEST_NUMBER_4);
-    push_front(test_list, TEST_NUMBER_5);
-    push_front(test_list, TEST_NUMBER_6);
+    pushFront(test_list, TEST_NUMBER_4);
+    pushFront(test_list, TEST_NUMBER_5);
+    pushFront(test_list, TEST_NUMBER_6);
 
 
-    assert(find_element(test_list, TEST_NUMBER_5, "T1") == 1);
-    assert(find_element(test_list, TEST_NUMBER_2, "T1") == 0);
+    assert(find_element(test_list, TEST_NUMBER_5) == 1);
+    assert(find_element(test_list, TEST_NUMBER_2) == 0);
 
     print_list(test_list);
     
-    list_free(&test_list);
+    list_free(test_list);
 }
 
 int main()
