68f6c7e6f4baf36933aaa1a169d05d73ce5fb807
Artem Shilov <142316977+Asterialka@users.noreply.github.com>
2024-05-20 16:40:16 +0300

    update header

--- tests/cache_test/generate_num.h
+++ tests/cache_test/generate_num.h
@@ -5,15 +5,21 @@
 #ifndef ARC_CASH_GENERAL_GENERATE_NUM_H
 #define ARC_CASH_GENERAL_GENERATE_NUM_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <limits.h>
 #include <time.h>
 
+/**
+ * @brief Generates a sequence of page references and writes them to a file.
+ *
+ * This function generates a sequence of page references based on the given parameters:
+ * - cache_size: the size of the cache.
+ * - n_size: the number of page references to generate.
+ * - max_page: the maximum page number.
+ * - file: the file to which the generated page references will be written. **/
+ 
+void generate(int cache_size, int n_size, int max_page, FILE *file);
 
-void generate(int cache_size, int n_size, int max_page, FILE *file)
-
-
-#endif //ARC_CASH_GENERAL_GENERATE_NUM_H
+#endif // ARC_CASH_GENERAL_GENERATE_NUM_H
